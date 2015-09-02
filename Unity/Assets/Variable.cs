736e44b5d62aae38440c546613f9acc99192b102
JamesBedore <hiphopdrummer88@gmail.com>
2015-09-02 15:58:23 -0600

    Variable and Function

---
+++ Unity/Assets/Variable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Variable : MonoBehaviour 
+{
+	int highFive = 5;
+
+	void Start(){
+		highFive = MultThree (highFive);
+		Debug.Log(highFive);
+}
+	int MultThree(int number){
+		int ret = number * 3;
+		return ret;
+}
+}
+	
